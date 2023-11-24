9cc5ea26b39a0026fce6ab2b097ca3da9c5368ac
YKMesso <B00142467@mytudublin.ie>
2023-11-24 21:35:16 +0000

    first commit

---
+++ src/app/api/register/route.js
@@ -0,0 +1,43 @@
+
+
+export async function GET(req, res) {
+
+  // Make a note we are on
+  // the api. This goes to the console.
+  console.log("in the api page")
+
+
+  // get the values
+  // that were sent across to us.
+  const { searchParams } = new URL(req.url)
+  const fname = searchParams.get('fname')
+  const lname = searchParams.get('lname')
+  const email = searchParams.get('email')
+  const pass = searchParams.get('pass')
+  const number = searchParams.get('number')
+  const dob = searchParams.get('dob')
+
+  console.log(fname);
+  console.log(lname);
+  console.log(email);
+  console.log(pass);
+  console.log(number);
+  console.log(dob);
+
+  // database call goes here
+  const { MongoClient } = require('mongodb');
+  const url = 'mongodb://root:example@localhost:27017/';
+  const client = new MongoClient(url);
+  const dbName = 'app'; // database name
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  const collection = db.collection('login'); // collection name
+  const findResult = await collection.insertOne({"username":
+    email, "pass": pass, "dob": dob})
+
+
+  // at the end of the process we need to send something back.
+  return Response.json({ "data":"true" })
+}
+
---
+++ src/app/api/removeFromCart/route.js
@@ -0,0 +1,36 @@
+import {MongoClient} from "mongodb";
+import app from "request";
+
+export async function GET(req, res) {
+    console.log("in the removeFromCart api page");
+    app.delete('/api/removeFromCart', (req, res) => {
+        // Logic to handle the DELETE request
+    });
+    console.log("DELETE funtion allowed");
+    // get the values that were sent across to us.
+    const { searchParams } = new URL(req.url);
+    const pname = searchParams.get('pname');
+    console.log(pname);
+//===================================================
+    const { MongoClient } = require('mongodb');
+    const url = 'mongodb://root:example@localhost:27017/';
+    const client = new MongoClient(url);
+    const dbName = 'app'; // database name
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db = client.db(dbName);
+    const collection = db.collection('shopping_cart'); // collection name
+    var deleteCriteria = { pname: pname, username: "sample@test.com" };
+    // Use deleteOne to remove the specified item from the collection
+    const deleteResult = await collection.deleteOne(deleteCriteria);
+
+    return Response.json({"data":"" + "removed" + ""})
+
+    if (deleteResult.deletedCount === 1) {
+        console.log('Item removed from cart:', pname);
+        return res.json({ "data": "removed" });
+    } else {
+        console.log('Item not found in cart:', pname);
+        return res.json({ "data": "not found" });
+    }
+}
