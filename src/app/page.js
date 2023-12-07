c75127ac45fa01afd6e5c9b21aaaa076ff19fd6e
YKMesso <B00142467@mytudublin.ie>
2023-12-07 15:05:39 +0000

    validator pt 1 w imports AND dialog inser

--- src/app/page.js
+++ src/app/page.js
@@ -21,7 +21,7 @@ import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/Dialog/Content;';
 import DialogContentText from '@mui/material/Dialog/DialogContextText';
-import DialogTitle from '/DialogTitle';
+import DialogTitle from '/@mui/material/DialogTitle';
 
 export default function Page() {
 
@@ -133,6 +133,29 @@ export default function Page() {
 
     return (
         <ThemeProvider theme={theme}>
+          <React.Fragment>
+            <Dialog
+                open={open}
+                onClose = {handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+                >
+              <DialogTitle id="alert-dialog-title">
+                {"Error"}
+              </DialogTitle>
+              <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                  {errorHolder}
+                </DialogContentText>
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleClose} autoFocus>
+                  Close
+                </Button>
+              </DialogActions>
+            </Dialog>
+          </React.Fragment>
+
           <Container component="main" maxWidth="xs">
             <CssBaseline/>
             <Box
