7ff0a084aed592c0c91005fcdbd3a7c7475abf9f
YKMesso <B00142467@mytudublin.ie>
2023-12-07 15:39:41 +0000

    password validator on reg & import

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -23,7 +23,11 @@ import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogActions from "@mui/material/DialogActions";
 
-
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 export default function Page() {
 
 
